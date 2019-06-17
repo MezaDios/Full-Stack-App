4f3b3aa8a5584232afe117c9769ac2ab72dce396
Ismael Meza <todoesculpadelgobierno@gmail.com>
2019-06-17 12:36:10 -0500

    Done

---
+++ server/src/index.ts
@@ -0,0 +1,39 @@
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+import userRoutes from './routes/userRoutes';
+
+class Server {
+
+    public app: Application
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    private config(): void {
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+    }
+
+    private routes(): void {
+        this.app.use('/api/users', userRoutes);
+    }
+
+    public start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log(`Server on port ${this.app.get('port')}`);
+        });
+    }
+}
+
+const server = new Server();
+server.start();
+
